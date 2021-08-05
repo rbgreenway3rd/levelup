3d95e4cf08ef9db210b3e9040b3a4b757c706259
rbgreenway3rd <3rdgreenway@gmail.com>
2021-08-05 14:22:38 -0500

    chapter 5

---
+++ levelupapi/models/game.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Game(models.Model):
+    name = models.CharField(max_length=100)
+    game_type = models.ForeignKey("GameType", on_delete=models.CASCADE)
+    description = models.CharField(max_length=150)
+    number_of_players = models.IntegerField()
+    gamer_id = models.ForeignKey("Gamer", on_delete=models.CASCADE)
+    maker = models.CharField(max_length=50)
